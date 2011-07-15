3b9aa6e54f2decc58181f4253a70d11ed14a5ca9
ryo hang <ryo@webs.com>
2011-07-15 16:01:47 -0400

    change to firefox driver

--- step4/features/environments/plain.rb
+++ step4/features/environments/plain.rb
@@ -3,7 +3,8 @@ Capybara.run_server = false
 #Capybara.app_host = "http://www.webs.com"
 Capybara.default_driver = :selenium
 Capybara.default_selector = :xpath
-Selenium::WebDriver.for :chrome
+#Run in google chrome mode
+#Selenium::WebDriver.for :chrome
 
 include RSpec::Matchers
 include Websautotest::Pagepattern
